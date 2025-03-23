967e0ded0b0228157009dd6fb8f0f634565476dc
Miguel <miguel.mamanig@ucsm.edu.pe>
2025-03-22 21:52:28 -0500

    feat: Se corrigió el error y se agregaron pruebas para el ejercicio 2

--- src/main/java/tudelft/ghappy/GHappy.java
+++ src/main/java/tudelft/ghappy/GHappy.java
@@ -6,7 +6,7 @@ public class GHappy {
         assert str!=null;
         for(int i = 0; i < str.length(); i++) {
             if(str.charAt(i) == 'g') {
-                if (i >= 0 && str.charAt(i-1) == 'g') { continue; }
+                if (i > 0 && str.charAt(i-1) == 'g') { continue; } //El error era el uso del "=", ya que el programa intenta leer una letra que no existe en la cadena recibida.
                 if (i+1 < str.length() && str.charAt(i+1) == 'g') { continue; }
                 return false;
             }
--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,25 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+    @Test
+    public void prueba_1() {
+        GHappy gh = new GHappy();
+        boolean resultado = gh.gHappy("ggxx");
+        Assertions.assertEquals(true, resultado);
+    }
+    @Test
+    public void prueba_2() {
+        GHappy gh = new GHappy();
+        boolean resultado = gh.gHappy("xggxgx");
+        Assertions.assertEquals(false, resultado);
+    }
+    @Test
+    public void prueba_3() {
+        GHappy gh = new GHappy();
+        boolean resultado = gh.gHappy("xgxx");
+        Assertions.assertEquals(false, resultado);
+    }
 }
