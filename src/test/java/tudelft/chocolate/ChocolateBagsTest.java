b175c41c99d39e2aab08cc0161af4b3f679121cf
Miguel <miguel.mamanig@ucsm.edu.pe>
2025-03-22 21:09:10 -0500

    feat: Se añadieron 2 pruebas para el ejercicio 1

--- src/test/java/tudelft/chocolate/ChocolateBagsTest.java
+++ src/test/java/tudelft/chocolate/ChocolateBagsTest.java
@@ -14,8 +14,23 @@ public class ChocolateBagsTest {
     }
 
     @ParameterizedTest(name = "small={0}, big={1}, total={2}, result={3}")
+    @CsvSource({ "3,2,10,0", "1,2,11,1", "3,2,15,-1", "1,4,25,-1" })
+    public void totalIsTooBig_2(int small, int big, int total, int expectedResult) {
+        ChocolateBags bags = new ChocolateBags();
+        int result = bags.calculate(small, big, total);
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    @ParameterizedTest(name = "small={0}, big={1}, total={2}, result={3}")
     @CsvSource({ "4,0,10,-1", "4,1,10,-1", "5,2,10,0", "5,3,10,0" })
     public void onlyBigBars(int small, int big, int total, int expectedResult) {
+        int result = new ChocolateBags().calculate(small, big, total);
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    @ParameterizedTest(name = "small={0}, big={1}, total={2}, result={3}")
+    @CsvSource({ "5,2,20,-1", "3,2,15,-1", "5,4,20,0", "5,3,15,0" })
+    public void onlyBigBars_2(int small, int big, int total, int expectedResult) {
         int result = new ChocolateBags().calculate(small, big, total);
         Assertions.assertEquals(expectedResult, result);
     }
