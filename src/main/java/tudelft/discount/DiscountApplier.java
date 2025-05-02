b2bf79d276c176b85b6e247f7850c0f233276944
Miguel <miguel.mamanig@ucsm.edu.pe>
2025-05-01 23:59:34 -0500

    feat: Se corrigio el error del Ejercicio 3

--- src/main/java/tudelft/discount/DiscountApplier.java
+++ src/main/java/tudelft/discount/DiscountApplier.java
@@ -9,15 +9,13 @@ public class DiscountApplier {
     }
 
     public void setNewPrices() {
-
         for(Product product : dao.all()) {
-            if(product.getCategory().equals("BUSINESS")) {
+            if(product.getCategory().equals("HOME")) { // Error en la categoria, cambiado por HOME
                 product.setPrice(product.getPrice() * 0.9);
             }
-            if(product.getCategory().equals("HOME")) {
-                product.setPrice(product.getPrice() * 1.1);
+            if(product.getCategory().equals("BUSINESS")) {
+                product.setPrice(product.getPrice() * 1.1); // Error en la categoria, cambiado por BUSINESS
             }
         }
-
     }
 }
--- src/test/java/tudelft/discount/DiscountApplierTest.java
+++ src/test/java/tudelft/discount/DiscountApplierTest.java
@@ -1,5 +1,53 @@
 package tudelft.discount;
 
+import org.junit.Test;
+import org.junit.Before;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
 public class DiscountApplierTest {
 
+    private ProductDao dao;
+    private DiscountApplier applier;
+
+    @Before
+    public void setup() {
+        dao = mock(ProductDao.class);
+        applier = new DiscountApplier(dao);
+    }
+
+    @Test
+    public void test1() {
+        Product homeProduct = new Product("Sofa", 100.0, "HOME");
+        when(dao.all()).thenReturn(Arrays.asList(homeProduct));
+
+        applier.setNewPrices();
+
+        assertEquals(90.0, homeProduct.getPrice(), 0.001);
+    }
+
+    @Test
+    public void test2() {
+        Product businessProduct = new Product("Printer", 200.0, "BUSINESS");
+        when(dao.all()).thenReturn(Arrays.asList(businessProduct));
+
+        applier.setNewPrices();
+
+        assertEquals(220.0, businessProduct.getPrice(), 0.001);
+    }
+
+    @Test
+    public void test3() {
+        Product homeProduct = new Product("Table", 150.0, "HOME");
+        Product businessProduct = new Product("Laptop", 1000.0, "BUSINESS");
+        when(dao.all()).thenReturn(Arrays.asList(homeProduct, businessProduct));
+
+        applier.setNewPrices();
+
+        assertEquals(135.0, homeProduct.getPrice(), 0.001);      // 150 * 0.9
+        assertEquals(1100.0, businessProduct.getPrice(), 0.001); // 1000 * 1.1
+    }
 }
